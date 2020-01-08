155659dc7c5538bc1a7c871650ffdf0be66cc3ea
ArlynC <alejandra321.any@gmail.com>
2020-01-08 11:02:14 -0500

    manipulacion

--- tema1upt/app/src/main/java/com/example/tema1upt/manipulacion1.java
+++ tema1upt/app/src/main/java/com/example/tema1upt/manipulacion1.java
@@ -3,12 +3,34 @@ package com.example.tema1upt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class manipulacion1 extends AppCompatActivity {
 
+    EditText edtnombre;
+    EditText edtmonto;
+    TextView txtresultado;
+    Button btnejecutar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manipulacion1);
+        edtnombre=findViewById(R.id.edtnombre);
+        edtmonto=findViewById(R.id.edtmonto);
+        txtresultado=findViewById(R.id.txtResultado);
+        btnejecutar=findViewById(R.id.btncambiar);
+        btnejecutar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double cambio;
+                cambio=Double.parseDouble(edtmonto.getText().toString())*3.3;
+                txtresultado.setText(edtnombre.getText() + " el monto de "+ edtmonto.getText()+
+                        " dolares equivale a " + cambio.toString() + "Soles");
+            }
+        });
     }
+
 }
